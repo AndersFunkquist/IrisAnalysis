0269bf9b255178d533c677ad41898ba6230de585
Anders Funkquist <anders.funkquist@regionhalland.se>
2023-11-28 11:08:57 +0100

    new changes

--- iristestset.R
+++ iristestset.R
@@ -30,3 +30,38 @@ set.seed(1000)
 
 s <- iris %>% slice_sample(n=15)
 ggpairs(s,aes(color=Species))
+
+install.packages('sampling')
+library(sampling)
+
+id2 <- strata(iris,stratanames = 'Species',size = c(5,5,5), method = 'srswor')
+id2
+
+s2 <- iris %>% slice(id2$ID_unit)
+ggpairs(s2,aes(color=Species))
+
+#install.packages('dplyr')
+library(dplyr)
+install.packages('plotly')
+library(plotly)
+
+plotly::plot_ly(iris,x=~Sepal.Length,y=~Petal.Length,z=~Sepal.Width,
+                size = ~Petal.Width, color = ~Species, type = 'scatter3d')
+
+pc <- iris %>% select(-Species) %>% as.matrix() %>% prcomp()
+summary(pc)
+plot(pc,type='line')
+str(pc)
+
+iris_projected <- as_tibble(pc$x) %>% add_column(Species=iris$Species)
+ggplot(iris_projected, aes(x=PC1,y=PC2,color=Species)) + geom_point()
+
+ggplot(iris_projected,aes(x=PC1,y=0,color=Species)) + geom_point() +
+  scale_y_continuous(expand = c(0,0)) + 
+  theme(axis.text.y = element_blank(), axis.title.y = element_blank())
+
+install.packages('factoextra')
+library(factoextra)
+
+fviz_pca(pc)
+fviz_pca_var(pc)
