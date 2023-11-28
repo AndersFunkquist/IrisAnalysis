9824ae24ebfea7c8f3b91a5a1799d2b51cf27788
Anders Funkquist <anders.funkquist@regionhalland.se>
2023-11-28 10:51:49 +0100

    a bit down in different plots

--- iristestset.R
+++ iristestset.R
@@ -14,3 +14,19 @@ install.packages('GGally')
 library(GGally)
 
 ggpairs(iris,aes(color=Species))
+
+clean.data <- iris %>% drop_na() %>% unique()
+summary(clean.data)
+
+iris %>% group_by(Species) %>% summarize_all(mean)
+iris %>% group_by(Species) %>% summarize_all(median)
+
+sample(c('A','B','C'), size = 10,replace = T)
+take <- sample(seq(nrow(iris)),size = 15)
+take
+iris[take,]
+
+set.seed(1000)
+
+s <- iris %>% slice_sample(n=15)
+ggpairs(s,aes(color=Species))
